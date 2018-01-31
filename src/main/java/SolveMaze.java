53445b14115f006e3e842fb3f35558d9aec424ea
dell-pc <haoyan3@illinois>
2018-01-30 18:24:57 -0600

    Hao Yan, Lab2

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,8 +34,17 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while ((isFinished) == false){
+                turnRight();
+                While (canMove() == false) {
+                    turnLeft;
+                }
+                move;
+            }
         }
+
+    }
+
 
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
